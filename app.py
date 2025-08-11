c569f7e07434de20bd26b98a9ae151a9f90a60cc
Naveen K <jaguarff66@gmail.com>
2025-08-11 11:49:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pickle
 import os
 
 # Load trained model
-MODEL_PATH = os.path.join(os.path.dirname(r"E:\Car_Price_Prediction\Car_Price_prediction\model.pkl"), "model.pkl")
+MODEL_PATH = os.path.join(os.path.dirname(r"E:\Car_Price_Prediction\Car_Price_prediction\model.pkl"))
 with open(MODEL_PATH, "rb") as f:
     model = pickle.load(f)
 
