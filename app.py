bdcd35d1c2b88b853450c92fd21a303206fa8394
Naveen K <jaguarff66@gmail.com>
2025-08-12 00:59:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ if st.button("Predict Price"):
         "Fuel_Type_numeric": Fuel_Type_numeric,
         "Transmission_numeric": Transmission_numeric
     }])
+    import numpy as np
     user_data = np.array([[120000,3,23,998,67,90,5,9,0,0,0]])
     y_p = model.predict(user_data)
     print(y_p[0])
